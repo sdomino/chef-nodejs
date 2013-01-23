bff57ecf76728db9f8659bb8dfde685d22d9aed9
Steve Domino <domino.steve@gmail.com>
2013-01-22 21:28:26 -0700

    first commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "chef-nodejs"
+maintainer       "YOUR_NAME"
+maintainer_email "YOUR_EMAIL"
+license          "All rights reserved"
+description      "Installs/Configures chef-nodejs"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
